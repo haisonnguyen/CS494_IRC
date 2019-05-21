aaf6d71fa029c0230895d49f7f49722840b23396
Haison Nguyen <hat@Haisons-MacBook-Pro.local>
2019-05-20 21:14:45 -0700

    figured out how to send to a room socket

--- index.js
+++ index.js
@@ -5,36 +5,26 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 8080;
 
-var chatRooms = {};
-var onlineUsers = {};
-chatRooms["main"] = "main";
-
 app.use(express.static(__dirname + "/public"));
+const main = "main_chat";
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   io.emit("user join", socket.id);
-  onlineUsers[socket.id] = socket;
-  socket.room = "main";
+  socket.join(main);
+
+  socket.on("chat message", data => {
+    console.log(data.room, data.msg)
+    io.to(data.room).emit("chat message", data);
+  });
+
+  socket.on("switch room", (room) => {
   
-  socket.on("chat message", msg => socket.in(socket.room).emit("chat message", msg));
-
-  socket.on("create chat room", (room_name) => {
-    chatRooms[room_name] = room_name;
-    socket.join(room_name);
-    socket.emit("chat room created", room_name);
-  });
-
-  socket.on('switch rooms', (room) => {
-    socket.leave(socket.room);
-    socket.room = room;
-  });
-
+    socket.join(room);
+  })
   socket.on("disconnect", () => {
-    --numUsers;
     io.emit("user left", socket.id);
-  });
+  })
 });
-
 
 server.listen(port, () => {
   console.log("Listening on port " + port);
--- public/socket.js
+++ public/socket.js
@@ -1,51 +1,59 @@
 // Start
-$(start = () => {
-  var socket = io('http://localhost:8080');
-  var chatRooms = {};
-  $("form").submit(function (e) {
-    e.preventDefault();
-    socket.emit("chat message", $("#m").val());
-    $("#m").val("");
-    return false;
-  });
+$(
 
-  $("#create-room").on('click', () => {
-    $('#create-chat-room-name').show();
-  });
-
-  $(".tab-links").on('click', () => {
-    var current = $(this).val();
-    switchRooms(current);
-  });
-
-  $('#enter-chat-room-name').on('click', () => {
-    var room_name = $('#chat-room-name').val();
-    $('#chat-room-name').val("");
-    socket.emit('create chat room', room_name);
-    $('#create-chat-room-name').hide();
-    $($('<button class="tab-links">').text(val)).insertBefore($('div button:last-child'));
-  });
-
-  socket.on('chat message', (msg) => {
-    $('#main_chat').append($('<li>').text(msg));
-  })
-
-  socket.on('chat room created', (chat_room_name) => {
-    var new_chat_room = io(chat_room_name);
-    chatRooms[chat_room_name] = new_chat_room;
-  });
   
-  socket.on('user join', (user) => {
-    $('#main_chat').append($('<li>').text(user.toString() + " has joined!"));
-  });
+  (start = () => {
+    var current = "main_chat";
+    var socket = io();
+    $("form").submit(function(e) {
+      e.preventDefault();
+      var message = $("#m").val();
+      socket.emit("chat message", {room: current, msg: message} );
+      $("#m").val("");
+      return false;
+    });
 
+    $("#create-room").on("click", () => {
+      $("#create-chat-room-name").show();
+    });
 
-  socket.on('user left', (user) => {
-    $('#main_chat').append($('<li>').text(user.toString() + " has left!"));
-  });
-});
+    $(".tab-links").on("click", () => {
+      var current = $(this).val();
+      switchRooms(current);
+    });
 
+    $("#enter-chat-room-name").on("click", () => {
+      var room_name = $("#chat-room-name").val();
+      $("#chat-room-name").val("");
+      socket.emit("create chat room", room_name);
+      $("#create-chat-room-name").hide();
+      $($('<button class="tab-links">').text(val)).insertBefore(
+        $("div button:last-child")
+      );
+    });
+
+    socket.on("chat message", data => {
+      console.log(data.room, data.msg)
+
+      var $room = data.room;
+      $(room).append($("<li>").text(data));
+    });
+
+    socket.on("chat room created", chat_room_name => {
+      var new_chat_room = io(chat_room_name);
+    });
+
+    socket.on("user join", user => {
+      $("#main_chat").append($("<li>").text(user.toString() + " has joined!"));
+    });
+
+    socket.on("user left", user => {
+      $("#main_chat").append($("<li>").text(user.toString() + " has left!"));
+    });
+  })
+);
 
 function switchRooms(room) {
-  socket.emit('switch rooms', room);
+  current = room;
+  socket.emit("switch rooms", room);
 }
