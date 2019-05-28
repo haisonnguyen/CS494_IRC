aa20909d74c49fdedd79ca4ce0e536d1e1ba7ee7
Haison Nguyen <hat@Haisons-MacBook-Pro.local>
2019-05-28 14:36:38 -0700

    updated front end with bootsrap, added create room modal

--- public/socket.js
+++ public/socket.js
@@ -1,41 +1,37 @@
+
+var room = '<button id="create-room-btn" class="btn-primary btn-group-vertical">name</button>';
 var current = "main";
 
 $(document).ready(() => {
-  // Start
+  console.log("Ready");
   var socket = io();
-  $("form").submit(function (e) {
-    e.preventDefault();
-    var message = $("#m").val();
-    socket.emit("chat message", { room: current, msg: message });
-    $("#m").val("");
-    return false;
-  });
 
-  $("#create-room").on("click", () => {
-    $("#create-chat-room-name").show();
-  });
-
-  $(".tab").on("click", ".tab-links", e => {
-    var id = e.target.id;
-    if (id != "create-room") {
-      current = id;
-      socket.emit("join room", current);
-    }
-  });
-
-  $("#enter-chat-room-name").on("click", () => {
+  $("#create-room-submit").on("click", () => {
     createRoom();
   });
-
+  
   $("#chat-room-name").keypress(e => {
     var key_code = e.keyCode ? e.keyCode : e.which;
     if (key_code == "13") createRoom();
   });
+  
+ 
+  $('#chat-message-form').submit(function (e) {
+    e.preventDefault();
+    var message = $("#chat-message-input").val();
+    $("#chat-message-input").val("");
+    socket.emit("chat message", { room: current, msg: message });
+    console.log(message);
+    return false;
+  });
 
   socket.on("chat message", data => {
     console.log(current, data.room, data.msg);
-    $("#chats #" + data.room).append($("<li>").text(data.msg));
+    $("#messages").append($("<li>").text(data.msg));
   });
+
+/*
+  
 
   socket.on("user join", user => {
     $("#chats #main").append($("<li>").text(user.toString() + " has joined!"));
@@ -45,22 +41,31 @@ $(document).ready(() => {
   socket.on("user left", user => {
     $("#chats #main").append($("<li>").text(user.toString() + " has left!"));
   });
+  
 
   function switchRooms(older, newer) {
     socket.emit("switch rooms", { old: older, newer: newer });
   }
-
+  */
   function createRoom() {
-    var room_name = $("#chat-room-name").val();
-    $("#chat-room-name").val("");
-    socket.emit("create room", room_name);
-    $("#create-chat-room-name").hide();
-    $(
-      $('<button class="tab-links" id="' + room_name + '">')
-        .text(room_name)
-        .insertBefore($("div button:last-child"))
-    );
-    $("#chats").append($('<ul class="messages" id="' + room_name + '"></ul>'));
-  };
+    var room_name = $('#chat-room-name').val();
+    $('#chat-room-name').val("");
+    var temp_room = room.replace("btn-primary","btn-light").replace("name",room_name);
+    $('#group-chat-list').append($('<li>').html(temp_room));
+  }
 });
 
+
+
+
+/*
+
+
+$(document).ready(() => {
+  // Start
+  
+
+  
+});
+
+*/
