89e26f2acb7b6f62a474346188af6e293e1e7e2c
Haison Nguyen <hat@Haisons-MacBook-Pro.local>
2019-06-09 15:28:23 -0700

    Finished requirements 2-14, including extra credit: private messaging

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const io = require("socket.io")(server);
 const port = process.env.PORT || 8080;
 
 var onlineUsers = {};
-var rooms = {};
 
 app.use(express.static(__dirname + "/public"));
 const main = "main";
 
+// Handle for when a client estbablishes a connection.
 io.on("connection", socket => {
+  // On connection, a client is automatically connected to "main"
   onlineUsers[socket.id] = socket;
   socket.join(main, () => console.log(socket.rooms));
   io.in(main).emit("user joined", { socketId: socket.id, roomName: main });
 
+  /**
+   * When a client wants to join a room, we make sure to check 
+   * if they're not already in the room
+   */
   socket.on("join room", roomName => {
     var found = searchRoom(socket, roomName);
     if (!found) {
@@ -26,6 +31,11 @@ io.on("connection", socket => {
     else socket.emit("err", "You're already in this room");
   });
 
+  /**
+   * When a client wants to leave a room, we make sure to check 
+   * that they're not attempting to leave a room they're
+   * not currently in
+   */
   socket.on("leave room", roomName => {
     var found = searchRoom(socket, roomName)
     if (found) {
@@ -36,6 +46,9 @@ io.on("connection", socket => {
     else socket.emit("err", "You are not in this room");
   });
 
+  /**
+   * List online members of a given room
+   */
   socket.on("list room members", roomName => {
     roomMembers = [];
     for (var key of Object.keys(onlineUsers)) {
@@ -46,29 +59,67 @@ io.on("connection", socket => {
     socket.emit("list room members", { roomName: roomName, roomMembers: roomMembers });
   });
 
+  /*
+  * Creating a room for all sockets
+  */
   socket.on("create room", roomName => {
     io.emit("created room", roomName);
   });
 
+  /**
+   * If a message starts with / it must be a special command
+   * We'll parse it then act on results
+   */
   socket.on("chat message", data => {
     if (data.msg.startsWith("/")) {
       specialCommands(socket, data);
     }
     else {
       io.in(data.roomName).emit("chat message", data);
-
     }
   });
 
-
-  socket.on("private chat message", data => {
-    io.sockets.socket(data.reciever).emit("private chat message", data.msg);
+  /**
+   * When creating a private message, we want to
+   * notify both parties of a creating a private message
+   */
+  socket.on("create private message", user => {
+    if(user == socket.id) {
+      socket.emit("err", "Why would you want to PM yourself");
+    }
+    else { 
+      socket.emit("created private message", user);
+      io.to(user).emit("created private message", socket.id);
+    }
   });
 
+  /**
+   * When sending a private message, we 
+   * 1. Notify sender that we sent msg
+   * 2. Notify receiver that a new msg is being sent
+   */
+  socket.on("send private message", (data) => {
+    //  io.to(`${socketId}`).emit('hey', 'I just met you');
+    let recipient = data.user;
+    socket.emit("sent private message", data);
+    data.user = socket.id;
+
+    io.to(recipient).emit("new private message", data);
+  });
+
+  /*
+  * When a socket disconnects, we want to properhaly handle
+  * disconnect() calls close()
+  */
   socket.on("disconnect", () => {
     socket.disconnect();
   });
 
+  /**
+   * If a user wants to disconnect, we
+   * cycle through all rooms of the user
+   * and notify those rooms that user left
+   */
   socket.on("disconnecting", () => {
     for (room of Object.keys(onlineUsers[socket.id].rooms)) {
       socket.to(room).emit("user left", { socketId: socket.id, roomName: room });
@@ -77,11 +128,14 @@ io.on("connection", socket => {
 
 });
 
-io
-
 server.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+
+/**
+ * Searches a socket's rooms for a specific roomName
+ * */ 
 
 function searchRoom(socket, roomName) {
   for (var room of Object.keys(onlineUsers[socket.id].rooms)) {
@@ -92,6 +146,13 @@ function searchRoom(socket, roomName) {
   return false;
 }
 
+  /********************************* Helper functions*********************************/
+
+  /**
+   * A user can handle special commands:
+   * 1. Multi chat room send: /send <room1,room2,...,roomx> <msg>
+   * 2. Multi chat room join: /join <room1,room2,...,roomx> 
+   */
 function specialCommands(socket, data) {
   let msg = data.msg;
   let socketId = data.socketId;
@@ -118,5 +179,8 @@ function specialCommands(socket, data) {
         io.in(room).emit("user joined", { socketId: socket.id, roomName: room });
       }
     }
+  }
+  else if (msg.startsWith("/disconnect")) {
+    socket.disconnect();
   }
 }
--- public/socket.js
+++ public/socket.js
@@ -2,7 +2,7 @@ let current = "main";
 
 $(document).ready(() => {
   // Establish a client connection
-  let socket = io.connect('//localhost:8080',{'forceNew':true });
+  let socket = io.connect("//localhost:8080", { forceNew: true });
   console.log("Ready");
 
   /**
@@ -19,31 +19,74 @@ $(document).ready(() => {
   let $createRoomButton = $("#createRoomButton");
   let $createRoomModal = $("#createRoomModal");
   let $groupChatList = $("#groupChatList");
+  let $onlineList = $("#onlineList");
+  let $privateChatList = $("#privateChatList");
+
   /**
    * Jquery handling events
    */
 
-  $groupChatList.on("click", "a", function () {
+   /**
+    * Entry point for when a user wants 
+    * to create a private message
+    */
+  $onlineList.on("click", "a", function() {
+    let user = $(this).text();
+    if (!$privateChatList.find($('#'+user)).length)
+      socket.emit("create private message", user);
+  });
+
+  /**
+   * Handles switching chat windows
+   * when a chat room is clicked
+   */
+  $groupChatList.on("click", "a", function() {
     switchChatWindows(this);
   });
 
+  /**
+   * Handles switching chat windows when
+   * a private message chat is clicked
+   */
+  $privateChatList.on("click", "a", function() {
+    switchChatWindows(this);
+  });
+
+  /**
+   * Switch rooms by hiding all siblings and showing current
+   */
   function switchChatWindows(room) {
     current = $(room).text();
-    $('#' + current + 'Messages').siblings().hide();
-    $('#' + current + 'Online').siblings().hide();
-    $('#' + current + 'Messages').show();
-    $('#' + current + 'Online').show();
+    $("#" + current + "Messages")
+      .siblings()
+      .hide();
+    $("#" + current + "Online")
+      .siblings()
+      .hide();
+    $("#" + current + "Messages").show();
+    $("#" + current + "Online").show();
   }
-  $createRoomButton.on("click", function () {
+
+  /** 
+   * Modal should hide once a room is created
+   */
+  $createRoomButton.on("click", function() {
     $createRoomModal.modal("show");
   });
 
+  /**
+   * When creating a room, we emit event + msg
+   * to server
+   */
   $("#createRoomSubmit").on("click", () => {
     let roomName = $chatRoomName.val();
     $chatRoomName.val("");
     socket.emit("create room", roomName);
   });
 
+  /**
+   * Same as directly above
+   */
   $("#chatRoomName").keypress(e => {
     let key_code = e.keyCode ? e.keyCode : e.which;
     if (key_code == "13") {
@@ -54,29 +97,47 @@ $(document).ready(() => {
     }
   });
 
-  $chatMessageForm.on("submit", function (e) {
+  /**
+   * Handles sending regular messages
+   */
+  $chatMessageForm.on("submit", function(e) {
     e.preventDefault();
     sendMessage();
   });
 
-  $sendMessageButton.on("click", function () {
+  /**
+   * Handles sending regular messages
+   */
+  $sendMessageButton.on("click", function() {
     sendMessage();
   });
 
-  $chatRoomList.on("click", "a > button", function () {
+  /**
+   * Button on group chat for joining/leaving chat
+   */
+  $chatRoomList.on("click", "a > button", function() {
     switchRooms(this);
   });
 
   /**
-   * Socket
+   * Socket stuf~~~
    */
 
-  socket.on("chat message", function (data) {
+   /**
+    * When a chat message arives,
+    * update the room
+    */
+  socket.on("chat message", function(data) {
     updateMessages(data);
   });
 
-  socket.on("user joined", function (data) {
-
+  /**
+   * User joined the room,
+   * 1. we notify the server
+   * 2. we announce arrival
+   * 3. we add user to online list
+   */
+  socket.on("user joined", function(data) {
     let user = data.socketId;
     let roomName = data.roomName;
     socket.emit("list room members", roomName);
@@ -93,8 +154,11 @@ $(document).ready(() => {
 
     $("#" + roomName + "Online").append(tag);
   });
-
-  socket.on("user left", function (data) {
+  
+  /**
+   * User left room so we announce their departure
+   */
+  socket.on("user left", function(data) {
     let user = data.socketId;
     let roomName = data.roomName;
     socket.emit("list room members", roomName);
@@ -103,30 +167,85 @@ $(document).ready(() => {
         .addClass("message list-group-item")
         .text(user + " has left!")
     );
+  });
 
-  })
-
-  socket.on("joined room", function (data) {
+  /**
+   * After emitting join room to server
+   * Server responds with joined room and
+   * we are able to join the room
+   */
+  socket.on("joined room", function(data) {
     joinRoom(data.roomName);
   });
 
-  socket.on("left room", function (data) {
+  /**
+   * Emit intent to leave room
+   * Server replies with left room
+   * we are able to leave room
+   */
+  socket.on("left room", function(data) {
     leaveRoom(data.roomName);
   });
+  
+  /**
+   * Emit even to create a room
+   * server responds w/ created room
+   * then create room
+   */
 
-  socket.on("created room", function (roomName) {
+  socket.on("created room", function(roomName) {
     createRoom(roomName);
   });
 
-  socket.on("list room members", function (data) {
+  /**
+   * Emit event for creating a message,
+   * we get this as a response and proceed
+   * with creating a private message
+   */
+  socket.on("created private message", function(user) {
+    createPrivateMessage(user);
+  });
+
+  /**
+   * List room members of a room
+   */
+  socket.on("list room members", function(data) {
     updateOnlineUsers(data);
+  });
+
+  /**
+   * New private message from some user
+   */
+  socket.on("new private message", function(data) {
+    console.log(data);
+    updatePrivateMessages(data);
+  });
+
+  /**
+   * The message we sent was emitted 
+   * to user by server
+   */
+  socket.on("sent private message", function(data) {
+    console.log(data); 
+    updatePrivateMessages(data);
+  });
+
+  /**
+   * Upon disconnection, we notify all users in main
+   */
+  socket.on("disconnect", () => {
+    $("#" + "main" + "Messages").append(
+      $("<a>")
+        .addClass("message list-group-item")
+        .text("You've been disconnected!")
+    );
   });
 
   socket.on("err", err => console.log(err));
 
 
+  /********************************* Helper functions*********************************/
   function createRoom(roomName) {
-
     let $listGroup = $("<div>").addClass("list-group");
     /**
      * Create the room's button for active chats
@@ -168,9 +287,9 @@ $(document).ready(() => {
 
   function leaveRoom(roomName) {
     $("#" + roomName + " > button").text("Join Room");
-    $('#' + roomName + 'Btn').remove();
-    $('#' + roomName + 'Messages').hide();
-    $('#' + roomName + 'Online').hide();
+    $("#" + roomName + "Btn").remove();
+    $("#" + roomName + "Messages").hide();
+    $("#" + roomName + "Online").hide();
   }
 
   function switchRooms(btn) {
@@ -178,7 +297,7 @@ $(document).ready(() => {
     let roomName = $btn.parent().attr("id");
     let state = $btn.text();
 
-    // Leave Room 
+    // Leave Room
     if (state == "Join Room") {
       socket.emit("join room", roomName);
     }
@@ -192,8 +311,55 @@ $(document).ready(() => {
     let message = $chatMessageInput.val();
     if (message) {
       $chatMessageInput.val("");
-      socket.emit("chat message", { roomName: current, msg: message });
+      if ($privateChatList.find($('#'+current)).length) {
+        console.log("found");
+        sendPrivateMessage(current, message);
+      }
+      else
+      {
+        console.log("sending to", current);
+        socket.emit("chat message", { roomName: current, msg: message });
+
+      }
     }
+  }
+
+  function createPrivateMessage(user) {
+    let $listGroup = $("<div>").addClass("list-group");
+    /**
+     * Create the room's button for active chats
+     */
+    let btn = $("<a>")
+      .addClass("list-group-item list-group-item-action list-group-room")
+      .attr("id", user)
+      .text(user);
+
+    $privateChatList.append(btn);
+    /**
+     * Create container for messages for specific room
+     */
+    $("#chatBoxList").append($listGroup.clone().attr("id", user + "Messages"));
+    switchChatWindows(btn);
+  }
+
+  function sendPrivateMessage(user, message) {
+    socket.emit("send private message", { user: user, msg: message });
+  }
+
+  
+
+  function updatePrivateMessages(data) {
+    let user = data.user;
+    let msg = data.msg;
+
+    $("#" + user + "Messages").append(
+      $("<a>")
+        .addClass("message list-group-item")
+        .text(msg)
+    );
+    $("#" + user + "Messages").animate({
+      scrollTop: $("#" + user + "Messages").prop("scrollHeight")
+    });
   }
 
   function updateMessages(data) {
@@ -204,13 +370,13 @@ $(document).ready(() => {
         .addClass("message list-group-item")
         .text(msg)
     );
-    $("#" + roomName + "Messages").animate({
+    $("#" +roomName + "Messages").animate({
       scrollTop: $("#" + roomName + "Messages").prop("scrollHeight")
     });
   }
 
   function updateOnlineUsers(data) {
-    $roomList = $('#' + data.roomName + 'Online');
+    $roomList = $("#" + data.roomName + "Online");
     $roomList.empty();
 
     for (member of data.roomMembers) {
@@ -218,14 +384,10 @@ $(document).ready(() => {
         .addClass("list-group-item list-group-item-action")
         .attr("id", member + "OnlineBtn")
         .text(member);
-      $roomList.append(tag)
+      $roomList.append(tag);
     }
   }
 
-  function updateRoomList() {
-
-  }
-
-  
-
+  // do later
+  function updateRoomList() {}
 });
